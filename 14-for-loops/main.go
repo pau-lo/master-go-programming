cd26d1adff5e741c377f8e6cb060cf0fdf6cecf2
pau-lo <pau_lo_lop_ez@protonmail.com>
2021-03-03 16:08:50 -0800

    Added if statements and for loops files.

---
+++ 14-for-loops/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// set i to 0, less than 10, increment
+	// intialization, boolean condition, post statement
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	// no while loop
+	// but this is similar to it
+	j := 10 // initialization
+	for j >= 0 {
+		fmt.Println(j)
+		j-- // post statement
+	}
+
+	// infinite loop
+	// sum := 0
+	// for {
+	// 	sum++
+	// }
+	// fmt.Println(sum) // never reached
+}
