20257b94327893e07b87fd7c23069078f6806ec5
pau-lo <pau_lo_lop_ez@protonmail.com>
2021-03-22 13:56:14 -0700

    created a for loop file.

--- 14-for-loops/main.go
+++ 14-for-loops/main.go
@@ -5,17 +5,17 @@ import "fmt"
 func main() {
 	// set i to 0, less than 10, increment
 	// intialization, boolean condition, post statement
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-	}
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(i)
+	// }
 
 	// no while loop
 	// but this is similar to it
-	j := 10 // initialization
-	for j >= 0 {
-		fmt.Println(j)
-		j-- // post statement
-	}
+	// j := 10 // initialization
+	// for j >= 0 {
+	// 	fmt.Println(j)
+	// 	j-- // post statement
+	// }
 
 	// infinite loop
 	// sum := 0
@@ -23,4 +23,11 @@ func main() {
 	// 	sum++
 	// }
 	// fmt.Println(sum) // never reached
+
+	for i := 0; i < 10; i++ {
+		if i%2 !=0 {
+			continue
+		}
+		fmt.Println(i) // if 'i' is an odd condition will not be met
+	}
 }
